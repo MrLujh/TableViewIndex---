4b16186a1032b985567cf65463b744675380cb2d
daniulaolu <287929070@qq.com>
2017-09-05 17:41:30 +0800

    Initial Commit

---
+++ TableViewIndexTests/TableViewIndexTests.m
@@ -0,0 +1,39 @@
+//
+//  TableViewIndexTests.m
+//  TableViewIndexTests
+//
+//  Created by lujh on 2017/9/5.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewIndexTests : XCTestCase
+
+@end
+
+@implementation TableViewIndexTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
