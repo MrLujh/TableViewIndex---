53e8eb2564bfd9cdc785cb0e22275ecae5c6a9ff
daniulaolu <287929070@qq.com>
2017-09-06 16:58:33 +0800

    taleview index
    taleview index

---
+++ TableViewIndex/ViewController/FriendModel.h
@@ -0,0 +1,17 @@
+//
+//  FriendModel.h
+//  TableViewIndex
+//
+//  Created by lujh on 2017/9/6.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FriendModel : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@property (nonatomic, copy) NSString *pic;
+
+@end
